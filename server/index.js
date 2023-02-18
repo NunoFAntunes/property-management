8b921322fef2b322f51d8224d48d2832256c5b55
Nuno Antunes <nunofcg.antunes@gmail.com>
2023-02-18 03:53:29 +0000

    Begin adding user registration functionality

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
+import { register } from "./controllers/auth.js";
+
 
 /* CONFIGS */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +36,11 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage: storage });
+
+
+/* ROUTES  WITH FILES */
+app.post('/auth/register', upload.single("picture"), register)
+
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
