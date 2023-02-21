ca700066dcb1515a23a16f49fdd65ad1444ea5a7
Nuno Antunes <nunofcg.antunes@gmail.com>
2023-02-21 03:55:05 +0000

    Adds auth functionality, permissions and users

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,3 +38,29 @@ export const register = async (req, res) => {
         res.status(500).json(error);
     }
 };
+
+
+/* LOGIN USER */
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email: email});
+        if (!user) {
+            return res.status(404).json({error: "User not found"});
+        };
+
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            return res.status(400).json({error: "Invalid password"});
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+
+        delete user.password;
+        res.status(200).json({token, user});
+
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+};
